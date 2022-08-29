4f8649836489f353455ea1a0e8579e3423f2be9b
Rohit <rohit.narkhede109@gmail.com>
2022-08-29 23:48:05 +1000

    adding TestCase04 with big input

--- LongestIncreasingSubsequence.Tests/LongestIncreasingSubsequenceComponentTest.cs
+++ LongestIncreasingSubsequence.Tests/LongestIncreasingSubsequenceComponentTest.cs
@@ -87,6 +87,17 @@ namespace LongestIncreasingSubsequence.Tests
             result.Should().Be("10298 10897 12291 15037 18446 23435 25333 27266");
 
         }
-               
+
+        [Fact]
+        public void TestCase04()
+        {
+            string inputString = "923 11613 30483 19569 24201 13461 1189 30793 8848 16914 16053 21700 22116 3852 20909 5231 31469 3862 16353 22813 28735 4421 3618 32303 9932 31892 7823 22547 28888 11143 11695 3339 2094 11023 9661 27440 7186 24750 15427 24502 31606 23515 3563 29553 12145 22184 11409 28824 6636 10658 21404 5578 27807 14073 13967 31310 3132 4321 7643 1951 13289 24375 17912 11304";
+            string result = _sut.GetEarliestLongestIncreasingSubsequence(inputString);
+
+            result.Should().NotBeEmpty();
+            result.Should().Be("3862 16353 22813 28735");
+
+        }
+
     }
 }
